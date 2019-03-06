d28f7a79f5349e43b215f4bd5af2645735b2c86f
Diego <diegodmx17@hotmail.com>
2019-03-06 12:03:43 -0500

    Hice la programacion de las flechas bajar y subir, cambie el archivo js.slim por el js con fx

---
+++ js/sitio.js
@@ -0,0 +1,41 @@
+/*-----------------------------
+INICIALIZANDO EL SMOOTH scroll
+------------------------------*/
+
+// smoothScroll.init({  Metodo antiguo, visto en Master en bootstrap
+//     speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+//     offset: 60, // Integer. How far to offset the scrolling anchor location in pixels
+//
+// });
+
+var scroll = new SmoothScroll('a[href="#servicios"]', {
+	speed: 1000,
+  offset: -23
+});
+
+var scroll = new SmoothScroll('a[href="#encabezado"]', {
+                          // ('a[href*="#"]') para seleccionar todos los a href
+	speed: 600
+});
+
+// offset: 50 esto es para generar el scroll solo hasta el numero en px que corresponde a la ventana
+
+/*-----------------------------
+-------------------------------
+------------------------------*/
+
+
+/*---------------------------------
+    OCULTAR Y MOSTRAR BOTON IR ARRIBA
+ ----------------------------------*/
+$(function () {
+    $(window).scroll(function () {
+        var scrolltop = $(this).scrollTop();
+        if (scrolltop >= 50) {
+            $(".ir-arriba").fadeIn(500); // cambie el fadeIn() por ese .hide()
+        } else {
+            $(".ir-arriba").fadeOut(); // cambie el fadeOut() por ese .show()
+        }
+    });
+
+});
